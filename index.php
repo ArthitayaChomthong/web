4fd4b8b4be1c5880c4f470b154da9f4ee53f786d
ArthitayaChomthong <76036837+ArthitayaChomthong@users.noreply.github.com>
2020-12-20 14:46:43 +0700

    Add files via upload

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
  <head>
   <title> ประวัติส่วนตัว </title>
  </head>
- <body>
-  <h1> TANGMAY </h1>
+ <body style="background-color:tomato;">
+  <h1 style="border:2px solid orange;"> ประวัติส่วนตัว </h1>
  </body>
 </html>
